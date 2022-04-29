099b6f684a7c2d2bf79ac5c3f5848d846076b9e7
Lewis Hammond <s1800707@connect.glos.ac.uk>
2022-04-29 17:57:15 +0100

    Remove editor usings

--- Assets/Scripts/BSPGraphVisualizer.cs
+++ Assets/Scripts/BSPGraphVisualizer.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BSPGraphVisualizer : MonoBehaviour
